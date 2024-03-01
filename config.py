6041f5c35c5b1765b3ffc1c8dcc73351b02c67eb
P4rsee <flandre495@foxmail.com>
2024-03-01 23:25:56 +0800

    modify config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 #encoding: utf-8
 import os
 
-HOSTNAME = '127.0.0.1'
-PORT     = '3306'
-DATABASE = 'course_management'
-USERNAME = 'root'
-PASSWORD = 'root1234'
-DB_URI = 'mysql+mysqldb://{}:{}@{}:{}/{}'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
+HOSTNAME = 'dpg-cngra2fsc6pc73b2sj90-a.singapore-postgres.render.com'
+PORT     = '5432'
+DATABASE = 'flask_cn45'
+USERNAME = 'flask_cn45_user'
+PASSWORD = 'RE1nMcdrvTma4tQwefeITrM6ZgbBDPxw'
+DB_URI = 'postgresql://{}:{}@{}:{}/{}'.format(USERNAME,PASSWORD,HOSTNAME,PORT,DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
